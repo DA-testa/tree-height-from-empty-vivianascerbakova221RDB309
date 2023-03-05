761dde3cffe325a95d2ec1b25f4f85567853ec9a
Viviana Scerbakova <125687697+vivianascerbakova221RDB309@users.noreply.github.com>
2023-03-05 15:00:23 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -70,5 +70,4 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-
 # print(numpy.array([1,2,3]))
